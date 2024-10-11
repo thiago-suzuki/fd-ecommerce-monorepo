fb73f3848475687eb677f2714d0b1ead6a9a8fc2
Thiago Ikenaga Suzuki <thiago.suzuki@gbmlogistica.com.br>
2024-10-11 16:13:11 -0300

    fix: fix port backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ? Number(process.env.PORT) : 3005
+
   const app = await NestFactory.create(AppModule, {cors: true});
-  await app.listen(3005, '0.0.0.0');
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server listening on port ${port}`)
 }
 bootstrap();
