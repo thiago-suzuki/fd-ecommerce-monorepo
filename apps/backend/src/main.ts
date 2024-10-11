3ad8cc64b53f7a7144652e2aeca8835af4977313
Thiago Ikenaga Suzuki <thiago.suzuki@gbmlogistica.com.br>
2024-10-11 17:44:04 -0300

    test: enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,6 +5,9 @@ async function bootstrap() {
   const port = process.env.PORT ? Number(process.env.PORT) : 3005
 
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   await app.listen(port, '0.0.0.0');
 
   console.log(`Server listening on port ${port}`)
